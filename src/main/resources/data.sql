89468ba2db920b25cfe8c7b52d35e6bc0a74b851
Anton Ganev <122820049+AntonGeorgievGanev@users.noreply.github.com>
2024-07-20 06:48:31 +0300

    data.sql for inserting initial review.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `reviews` (`author`, `description`, `published`, `rating`, `title`, `user_id`)
+VALUES ("Anton Ganev", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+ molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+ numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+ optio, eaque rerum!", "2024-07-20", 5, "Excellent", 1);
